500ee8c95afea27afb6705c6fe1e08f0fafd8333
markmmelekhin <75304878+markmmelekhin@users.noreply.github.com>
2021-01-05 20:11:35 +0300

    Add files via upload

--- VkPostsCollector/ApplicationLayer/Configs.cs
+++ VkPostsCollector/ApplicationLayer/Configs.cs
@@ -7,10 +7,10 @@ namespace VkPostsCollector.ApplicationLayer
 {
     public static class Configs
     {
-        public static string AccessToken { get; set; } = "20e0e7e620e0e7e620e0e7e6e420901b4d220e020e0e7e67e72e2ea810c2598e177d57b"; // Тут можно например ключ приложения вк, которое тоже надо создать: https://vk.com/apps?act=manage
-        public static string TelegramToken { get; set; } = "830931210:AAFimSLygGF7heGe072dseukLAnwIBUI4BU"; // Токен бота в телеге
-        public static string AdLink { get; set; } = "https://alitems.com/g/1e8d114494bd0482fb1316525dc3e8/";
-        public static string TelegramChannel { get; set; } = "@clickandbuy";
+        public static string AccessToken { get; set; } = "44d77fd344d77fd344d77fd30644a69363444d744d77fd31b1035924bb1683e0a9f68e5"; // Тут можно например ключ приложения вк, которое тоже надо создать: https://vk.com/apps?act=manage
+        public static string TelegramToken { get; set; } = "1234245036:AAG7KU0QvRCWMSW-YfjNa7o_lqwifVYAZWI"; // Токен бота в телеге
+        public static string AdLink { get; set; } = "https://alitems.com/g/1e8d114494713ce4258416525dc3e8/";
+        public static string TelegramChannel { get; set; } = "@yafilantrop";
         
         public static List<GroupDTO> Groups = new List<GroupDTO>();
         public static PublicationFilters PublicationFilters = new PublicationFilters();
@@ -53,6 +53,10 @@ namespace VkPostsCollector.ApplicationLayer
             {
                 FiltersSerializer.Serialize(fs, PublicationFilters);
             }
+        }
+
+        internal class PublicationFilters
+        {
         }
     }
 }
